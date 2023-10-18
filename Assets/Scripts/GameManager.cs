272ef3b72ea22701ea4ef1309a917cea3a3e6de1
Lê Thanh Phát <ltphatt2709@gmail.com>
2023-10-18 20:38:45 +0700

    Update Level Selector Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,17 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("Button sound")]
+    [SerializeField] AudioClip btnSound;
+    [SerializeField][Range(0f, 1f)] float btnSoundVolume = 1f;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
-        Time.timeScale = 1f;
+        Time.timeScale = 2f;
     }
 
     public void StartGame()
@@ -24,5 +28,14 @@ public class GameManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PlayButtonClickSound()
+    {
+        if (btnSound != null)
+        {
+            Vector3 camPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(btnSound, camPos, btnSoundVolume);
+        }
     }
 }
